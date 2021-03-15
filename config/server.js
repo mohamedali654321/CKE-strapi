ce1335b73368c38f088c219f42a7a0d05dcd4c85
mohamedali654321 <mmmmali654321@gmail.com>
2021-03-15 14:05:57 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7ecf7354348c08b09bc67d4d4278ac62'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'dfef0c56-722f-4643-b255-4e1581ac811b'
+};
